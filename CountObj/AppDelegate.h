4bafd826ef2d4f04866e584928599c04dcb1d734
Thiago <thiago@tecnonutri-mac-mini.local>
2015-09-14 09:22:23 -0300

    Initial Commit

---
+++ CountObj/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CountObj
+//
+//  Created by Thiago on 9/14/15.
+//  Copyright (c) 2015 Thiago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CountObj/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CountObj
+//
+//  Created by Thiago on 9/14/15.
+//  Copyright (c) 2015 Thiago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
